44fd8687e40611768bee829dee6013c0cf198298
Michael E. Cotterell <mepcotterell@gmail.com>
2018-01-23 09:03:19 -0500

    updated echo server code

--- echo_server/echo_server.py
+++ echo_server/echo_server.py
@@ -1,9 +1,29 @@
 
 import socket
+import threading
 
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 4760 # use one of your assigned ports instead
 SERVER_ADDR = (SERVER_HOST, SERVER_PORT)
+
+class HandlerThread(threading.Thread):
+    """TODO write docstring!"""
+
+    def __init__(self, client, address):
+        """constructor"""
+        threading.Thread.__init__(self)
+        self.client = client
+        self.address = address
+
+    def run(self):
+        """thread code"""
+        print('executing thread for client {}'.format(address))
+        data = self.client.recv(4096)
+        print('received {} of length {}'.format(data, len(data)))
+        self.client.send(data)
+        print('sent echo back')
+        self.client.shutdown(socket.SHUT_RDWR)
+        self.client.close()
 
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -12,11 +32,6 @@ connection.listen(10)
 
 while True:
     client, address = connection.accept()
-    data = client.recv(4096)
-    print('received {} of length {}'.format(data, len(data)))
-    client.send(data)
-    print('sent echo back')
-    client.shutdown(socket.SHUT_RDWR)
-    client.close()
-
+    th = HandlerThread(client, address)
+    th.start()
 
